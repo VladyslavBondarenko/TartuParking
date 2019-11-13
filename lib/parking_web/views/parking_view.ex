3ec455120f0831a3cdc9ed541c89c97306724650
Vladyslav Bondarenko <vlbond@ttu.ee>
2019-11-13 19:37:59 +0200

    fix(backend) fix api output

--- lib/parking_web/views/parking_view.ex
+++ lib/parking_web/views/parking_view.ex
@@ -3,11 +3,11 @@ defmodule ParkingWeb.ParkingView do
   alias ParkingWeb.ParkingView
 
   def render("index.json", %{parkings: parkings}) do
-    %{data: render_many(parkings, ParkingView, "parking.json")}
+    %{parkings: render_many(parkings, ParkingView, "parking.json")}
   end
 
   def render("show.json", %{parking: parking}) do
-    %{data: render_one(parking, ParkingView, "parking.json")}
+    render_one(parking, ParkingView, "parking.json")
   end
 
   def render("parking.json", %{parking: parking}) do
