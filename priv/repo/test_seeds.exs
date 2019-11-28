b606387228112856420db9d44035049671644fdd
Vladyslav Bondarenko <vlbond@ttu.ee>
2019-11-28 14:13:55 +0200

    fix(api tests) seed parkings

--- priv/repo/test_seeds.exs
+++ priv/repo/test_seeds.exs
@@ -10,20 +10,24 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Parking.{Repo, UserManager, ParkingManager, Street, Zone}
+alias Parking.{Repo, UserManager, Zone}
 
 # seed customers
 [%{id: 1, username: "ivan", password: "123"}]
 |> Enum.map(fn user_data -> UserManager.create_user(user_data) end)
 
-# seed parkings
-[%{location: "58.382548,26.709504", timelimit: 0, area: "58.38273009407792,26.70879321461109 58.3822857547534,26.709758809856453 58.38238137254904,26.709962657741585 58.38281446167178,26.708943418315926"}]
-|> Enum.map(fn parking_data -> ParkingManager.create_parking(parking_data) end)
-
 # seed zones
-zoneP = Repo.insert!(%Zone{name: "pedestrian", hourPayment: 0.0, realTimePayment: 0.0})
-zoneA = Repo.insert!(%Zone{name: "A", hourPayment: 2.0, realTimePayment: 0.16})
-zoneB = Repo.insert!(%Zone{name: "B", hourPayment: 1.0, realTimePayment: 0.08})
+zoneA = Repo.insert!(%Zone{name: "A", hourPayment: 2.0, realTimePayment: 0.16, freeFirstMinutes: 0})
+zoneB = Repo.insert!(%Zone{name: "B", hourPayment: 1.0, realTimePayment: 0.08, freeFirstMinutes: 0})
+zoneF = Repo.insert!(%Zone{name: "free", hourPayment: 0.0, realTimePayment: 0.0, freeFirstMinutes: 0})
+
+# seed parkings
+[%{capacity:  20, location: "58.387746,26.696940", timelimit: 0, area: "58.38811045707349,26.697406020617336 58.38760994822571,26.696209755396694 58.387022263039675,26.69730409667477 58.387601512622474,26.698371615862698"},
+ %{capacity:  20, location: "58.379579,26.708482", timelimit: 0, area: "58.37976392189816,26.70835325396729 58.379589546902395,26.70809576190186 58.37939829591593,26.70855710185242 58.379592359409166,26.70881995833588"},
+ %{capacity:  20, location: "58.382548,26.709504", timelimit: 0, area: "58.38273009407792,26.70879321461109 58.3822857547534,26.709758809856453 58.38238137254904,26.709962657741585 58.38281446167178,26.708943418315926"},
+ %{capacity:  20, location: "58.378390,26.738600", timelimit: 0, area: "58.37843010866203,26.738337120747474 58.37807571936761,26.73843904469004 58.37845260944946,26.739291987156776"}, #nearest_by_path in test
+ %{capacity:  20, location: "58.375391,26.736067", timelimit: 0, area: "58.37555082370519,26.73658919121317 58.375221721192396,26.73431467796854 58.374642268281995,26.735285637631932 58.375221721192396,26.736685750737706"} #nearest_by_distance in test
+] |> Enum.map(fn data -> Repo.insert!(Ecto.build_assoc(zoneF, :parkings, data)) end)
 
 # seed streets
 streetsZoneA = [
